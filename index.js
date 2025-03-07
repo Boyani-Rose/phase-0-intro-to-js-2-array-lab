eb0cf3651408bf7eddf02634e97821e7d1edde45
Rose Momanyi <rosebmomanyi@gmail.com>
2025-03-07 17:48:07 +0300

    Learn about arrays

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+console.log (cats)
+
+function destructivelyAppendCat (name) {
+    cats.push (name)
+}
+
+console.log (cats)
+
+function destructivelyRemoveLastCat () {
+    cats.pop()
+}
+console.log (cats)
+
+function destructivelyPrependCat (name) {
+    cats.unshift(name)
+}
+
+ 
+function destructivelyRemoveFirstCat () {
+    cats.shift ()
+}
+console.log (cats)
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice (0, -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
